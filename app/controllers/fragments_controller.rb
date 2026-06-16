964bb155ee460a64fd4ca3806f9d0b979836b8fc
parikesitad-pm <parikesit.ad@gmail.com>
2026-06-16 14:35:36 +0700

    addTailwindCSS

---
+++ app/controllers/fragments_controller.rb
@@ -0,0 +1,25 @@
+class FragmentsController < ApplicationController
+  def index
+    @fragments = Fragment.order(created_at: :desc)
+  end
+
+  def new
+    @fragment = Fragment.new
+  end
+
+  def create
+    @fragment = Fragment.new(fragment_params)
+
+    if @fragment.save
+      redirect_to fragments_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def fragment_params
+    params.require(:fragment).permit(:content)
+  end
+end
